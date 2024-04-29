bdf5a95274487ac1f1b93b0fb4227340756e6267
eviva2000 <arjomandielaheh@gamin.com>
2024-04-29 22:01:13 +0200

    added basic structure

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useReducer } from "react";
+import Header from "./Header";
+import Main from "./Main";
+import Loader from "./Loader";
+import Error from "./Error";
+import StartScreen from "./StartScreen";
+const App = () => {
+  const initialState = {
+    questions: [],
 
-function App() {
+    //'loading', 'error,' 'ready', 'active', 'finished'
+    status: "loading",
+  };
+  const reducer = (state, action) => {
+    switch (action.type) {
+      case "dataRecieved":
+        return {
+          ...state,
+          questions: action.payload,
+          status: "ready",
+        };
+      case "dataFailed":
+        return {
+          ...state,
+          status: "error",
+        };
+      default:
+        throw new Error("Invalid action type");
+    }
+  };
+
+  const [{ questions, status }, dispatch] = useReducer(reducer, initialState);
+  const getQuestions = async () => {
+    try {
+      const response = await fetch("http://localhost:8000/questions");
+      const data = await response.json();
+      dispatch({ type: "dataRecieved", payload: data });
+    } catch (error) {
+      dispatch({ type: "dataFailed" });
+    }
+  };
+  useEffect(() => {
+    getQuestions();
+  }, []);
+
+  const numQuestions = questions.length;
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <Header />
+      <Main>
+        {status === "loading" && <Loader />}
+        {status === "error" && <Error />}
+        {status === "ready" && <StartScreen numQuestions={numQuestions} />}
+      </Main>
     </div>
   );
-}
+};
 
 export default App;
